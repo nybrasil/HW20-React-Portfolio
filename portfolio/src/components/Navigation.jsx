47ef4935072cb6546f1c18459a3ed59be2d171a8
Tania Bezerra <nybrasil@gmail.com>
2024-06-10 20:31:56 -0400

    started Navigation.jsx

---
+++ portfolio/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+// src/components/Navigation.jsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact activeClassName="active">About Me</NavLink></li>
+        <li><NavLink to="/portfolio" activeClassName="active">Portfolio</NavLink></li>
+        <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+        <li><NavLink to="/resume" activeClassName="active">Resume</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
