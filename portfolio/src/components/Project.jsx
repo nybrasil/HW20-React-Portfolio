9342e7fecb888bcd23d80634a7eb7a878ee4200b
Tania Bezerra <nybrasil@gmail.com>
2024-06-10 20:44:25 -0400

    started Project.jsx

---
+++ portfolio/src/components/Project.jsx
@@ -0,0 +1,17 @@
+// src/components/Project.jsx
+import React from 'react';
+
+const Project = ({ title, image, deployedUrl, repoUrl }) => {
+  return (
+    <div className="project">
+      <h3>{title}</h3>
+      <img src={image} alt={`${title} screenshot`} />
+      <p>
+        <a href={deployedUrl} target="_blank" rel="noopener noreferrer">Live Site</a> | 
+        <a href={repoUrl} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+      </p>
+    </div>
+  );
+};
+
+export default Project;
