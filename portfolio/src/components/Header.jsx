05aaa2244b8b2fa2bb11ebfd5a7200128bd08465
Tania Bezerra <nybrasil@gmail.com>
2024-06-10 20:27:18 -0400

    start Header.jsx

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,14 @@
+// src/components/Header.jsx
+import React from 'react';
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Tania Bezerra</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
