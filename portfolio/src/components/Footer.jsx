0de94df4cccdf08e82eaf541c22f38fa350bf584
Tania Bezerra <nybrasil@gmail.com>
2024-06-11 21:55:25 -0400

    components/Footer.jsx

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/nybrasil" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://linkedin.com/in/TaniaBezerra" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+     
+    </footer>
+  );
+};
+
+export default Footer;
