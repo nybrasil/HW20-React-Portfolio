a96ce12adbbe774096b1a980e4b805aaa0bbd9a2
Tania Bezerra <nybrasil@gmail.com>
2024-06-11 22:16:52 -0400

    created pages/AboutMe.jsx

---
+++ portfolio/src/pages/AboutMe.jsx
@@ -0,0 +1,14 @@
+// src/pages/AboutMe.jsx
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="path/to/photo.jpg" alt="Developer" />
+      <p>Short bio about the developer.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
